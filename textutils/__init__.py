bebd92fb8d73aa85b1ca8a36bbd467570b653bad
Patrick <58388327+8Patrick8@users.noreply.github.com>
2026-08-17 00:26:42 +0200

    Bibliotheksskelett mit Paket und Stubs erstellen (#7)
    * feat: Bibliotheksskelett mit Paket und Stubs erstellen
    
    Closes #3
    
    * refactor: review iteration 1
    
    Ref #3
    
    ---------
    
    Co-authored-by: office-crew <development@office-crew.dev>

---
+++ textutils/__init__.py
@@ -0,0 +1,13 @@
+from textutils.is_palindrome import is_palindrome
+from textutils.reverse_words import reverse_words
+from textutils.slugify import slugify
+from textutils.truncate import truncate
+from textutils.word_count import word_count
+
+__all__ = [
+    "is_palindrome",
+    "reverse_words",
+    "slugify",
+    "truncate",
+    "word_count",
+]
